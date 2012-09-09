8731b86aa73cfc525fd73e7e96d44df3686c16d1
Chandan <chandan.gupta@gmail.com>
2012-09-09 12:09:51 +0530

    Added placeholder for prefixSum.c

---
+++ prefixSum.c
@@ -0,0 +1,12 @@
+/*
+ * Author: Chandan Kumar chandan.kumar@students.iiit.ac.in
+ * Date: 2012-09-09
+ *
+ * Assignment 1.3 of Parallel Programming course during 
+ * Monsoon 2012 semester offered by Suresh Purini.
+ * 
+ * Prefix Sum Problem.
+ *
+ */
+
+#include <stdio.h>
