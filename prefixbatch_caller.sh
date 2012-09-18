47e318dddd1b19faee9e4ee5b6b01e8c0a018c50
Chandan <chandan.gupta@gmail.com>
2012-09-18 12:16:44 +0530

    Removed shell scripts from outer folder.

--- prefixbatch_caller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -f output.txt
-./one_caller.sh './prefixSum -n 268435456 -basic -noio -noinit' >> output.txt
-./one_caller.sh './prefixSum -n 1073741824 -basic -noio -noinit' >> output.txt
-./one_caller.sh './prefixSum -n 268435456 -cacheob -noio -noinit' >> output.txt
-./one_caller.sh './prefixSum -n 1073741824 -cacheob -noio -noinit' >> output.txt
-
-
-cat output.txt | grep Performance > better_output.txt
